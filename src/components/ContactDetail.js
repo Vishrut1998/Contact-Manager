96e4687e4b2b99beed047a5d6d251d2f978e1565
unknown <sharmavishrut1998@gmail.com>
2022-03-18 12:38:24 +0530

    Init Commit

---
+++ src/components/ContactDetail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import user4 from "../images/user4.png";
+
+const ContactDetail = (props) => {
+    const {name,email} = props.location.state.contact;
+    return (
+        <div className="main">
+            <div className="ui card centered">
+                <div className="imageContactDetail">
+                    
+                </div>
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div className="description">{email}</div>
+                </div>
+            </div>
+            <div className="center-div">
+                <Link to="/"><button className="ui button blue center" style={{marginLeft: "850px"}}>Back to Contact List</button>
+                </Link>
+            </div>
+        </div>
+    );
+
+}
+
+export default ContactDetail;
